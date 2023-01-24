c87081292f7fe37349e04898ab0fcd8f7a2c3e4a
bartekglowacz <bartekglowacz@gmail.com>
2023-01-24 19:39:04 +0100

    Pliki pomocnicze do testowania funkcji i do optymalizacji kodu

--- tmp_roboczy.py
+++ tmp_roboczy.py
@@ -1,8 +1,18 @@
-file = open("frequencies_txt", "r")
-frequency = file.readline()
+import time
+import math
 
-while frequency:
-    if frequency == "\n":
-        break
-    frequency = file.readline()
-    print(f"wartość podana do generatora: {frequency}\n")
+start_time = time.perf_counter()
+
+file = open('frequencies_txt', 'r')
+frequency = file.read().splitlines()
+print(f"Częstotliwości wysłane na generator: {frequency} MHz")
+
+
+for x in range(0, len(frequency), 1):
+    print(f"Częstotliwość nr {x+1}: {frequency[x]}")
+
+file.close()
+
+end_time = time.perf_counter()
+
+print(f"Elapsed time: {(end_time - start_time)*math.pow(10, 6)} us")
