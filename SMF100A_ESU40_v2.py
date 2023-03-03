17e347cb0b7898758221cdb2602a428202e47ef5
bartekglowacz <bartekglowacz@gmail.com>
2023-03-03 14:44:57 +0100

    dodano zapisywanie do pliku, gdzie użytkownik może zdefiniować jego nazwę. Dodano również zamienianie kropek na przecinki w zapisie liczbowym oraz rozdizelanie kolumn z częstotliwością od poziomu napięcia za pomocą tabulatora.

--- SMF100A_ESU40_v2.py
+++ SMF100A_ESU40_v2.py
@@ -1,7 +1,22 @@
+import datetime
 import time
 import pyvisa
 import SMF100A_preset
 import ESU40_preset
+
+
+def result_file_name(name_of_file):
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = "%02d" % now.month
+    day = "%02d" % now.day
+    hour = "%02d" % now.hour
+    minute = "%02d" % now.minute
+    second = "%02d" % now.second
+    prefix_name = year + month + day + "_" + hour + minute + second + "_"
+    full_name_of_file = prefix_name + name_of_file + ".txt"
+    return full_name_of_file
+
 
 
 def set_frequency():
@@ -13,10 +28,8 @@ def set_frequency():
     return frequency
 
 
-def wait(pause):
-    print("Podaj czas pomiaru w milisekundach")
-    pause = float(input()) / 1000
-    return pause
+def wait(measurement_pause):
+    time.sleep(measurement_pause / 1000)
 
 
 class Device:
@@ -40,7 +53,8 @@ class Device:
         # print(self.frequency_band)
 
 
-results = []
+esu_results = []
+smf_results = []
 
 # tworzenie klasy ESU40
 ESU40 = Device("ESU40", "10.0.0.4", "frequencies.txt")
@@ -55,10 +69,14 @@ SMF100A.IDN()
 SMF100A.frequencies_swapping()
 
 # ustawienie wstępne SMF 100A:
-
-SMF100A_preset.set_attenuator(10)
+SMF100A_preset.set_auto_attenuator()
+print(f"Wprowadź wartość tłumika na {SMF100A.name}")
+attenuator_value = input()
+SMF100A_preset.set_attenuator(attenuator_value)
 SMF100A_preset.select_unit("dBuV")
-SMF100A_preset.set_level(55)
+print(f"Wprowadź wartość poziomu napięcia na {SMF100A.name}")
+level = float(input())
+SMF100A_preset.set_level(level)
 SMF100A_preset.output_on_off("ON")
 
 # ustawienie wstępne ESU40:
@@ -74,9 +92,12 @@ ESU40_preset.set_span()
 
 frequency = set_frequency()
 print(f"{frequency = }")
+print("Podaj czas pomiaru w milisekundach")
+pause = float(input())
+
 for x in frequency:
     SMF100A.name_connected.write(f"FREQ {x} MHz")
-    # SMF100A_freq = float(SMF100A.name_connected.query('FREQ?')) / 10 ** 6  # odczytywanie częstotliwości SMF 100A
+    SMF100A_freq = float(SMF100A.name_connected.query('FREQ?')) / 10 ** 6  # odczytywanie częstotliwości SMF 100A
     ESU40.name_connected.write(f"FREQ:CENT {x} MHz")
     ESU40_preset.set_RefLevel()
     ESU40_preset.set_RBW()
@@ -85,17 +106,23 @@ for x in frequency:
     # ESU_freq = float(ESU40.name_connected.query('SENSe:FREQuency:CENTer?')) / 10 ** 6  # odczytywanie częstotliwości ESU40
     ESU_level = float(ESU40.name_connected.query_ascii_values('CALC:MARK1:Y?')[0])
     # print(f"f na {SMF100A.name}: {x}")
-    # print(f"f z ekranu SMF: {SMF100A_freq} MHz")
+    print(f"f z ekranu SMF: {SMF100A_freq} MHz")
     # print("poziom generatora: ", SMF100A.name_connected.query("POW?")) # odczytywanie wartości napięcia z ekranu SMF 100A
     # print(f"f na {ESU40.name}: {x}")
     # print(f"f z ekranu ESU: {ESU_freq} MHz")
-    print("Poziom odczytany z ESU40: ", ESU_level)  # odczytywanie poziomy sygnału z ESU40
-    time.sleep(0.2)
-    results.append(x)
-    results.append(ESU_level)
-print(results)
-results_txt = open("results.txt", "w")
-for x in results:
-    results_txt.write(str(x).splitlines(","))
+    print("U z ekranu ESU40: ", ESU_level)  # odczytywanie poziomy sygnału z ESU40
+    wait(pause)
+    smf_results.append(x)
+    esu_results.append(ESU_level)
+
+# Tworzenie pliku z wynikami
+print("Wprowadź nazwę pliku: ")
+file_name = input()
+final_file_name = result_file_name(file_name)
+final_results_txt = open(f"{final_file_name}", "w")
+
+final_results_txt.write("f [MHz]\tU [dBuV]\n")
+for x in range(0, len(smf_results)):
+    final_results_txt.write(str(smf_results[x]).replace(".", ",") + "\t" + str(esu_results[x]).replace(".", ",") + "\n")
 
 SMF100A_preset.output_on_off("OFF")
