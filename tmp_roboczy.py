84cd809bf960dfbdb9625b5e2d47d8a917265525
bartekglowacz <bartekglowacz@gmail.com>
2023-03-07 16:27:01 +0100

    .

--- tmp_roboczy.py
+++ tmp_roboczy.py
@@ -1,34 +1,21 @@
-import random
-import statistics as stat
+import time
 
-with open("C:\\Users\\bglowacz\\PycharmProjects\\Praca_IL-PIB\\frequencies_txt", "r") as frequencies_file:
-    frequencies = frequencies_file.readlines()
+import matplotlib.pyplot as draw
 
-frequencies = [float(x) for x in frequencies]
-level = []
-result_list = []
 
-for x in frequencies:
-    random_number = random.randint(1, 100)
-    level.append(random_number)
-print("Oryginalne poziomy: ", level)
+def drawing(x_value, y_value):
+    draw.ion()
+    draw.grid()
+    draw.xlabel('f [MHz]')
+    draw.ylabel('U [dBuV]')
+    draw.xscale('log')
+    draw.plot(x_value, y_value)
+    time.sleep(1)
 
-standard_deviation = stat.pstdev(level)
-print(f"Odchylenie standardowe wynosi: {standard_deviation}")
-median = stat.median(level)
-print(f"Mediana wynosi: {median}")
 
-for x in range(0, len(frequencies)):
-    while level[x] < median:
-        random_number = random.randint(1, 100)
-        level[x] = random_number
-        str(level[x]).replace(str(x), str(random_number))
-
-    result_list.append(str(frequencies[x]) + "  " + str(level[x]) + "")
-print(f"Nowe poziomy, z zamienionymi wartościami poniżej mediany: {level}")
-print(result_list)
-
-result_file = open("result.txt", "w")
-for x in range(0, len(frequencies)):
-    result_file.write(result_list[x])
-
+while True:
+    print("Podaj x: ")
+    x = int(input())
+    print("Podaj y: ")
+    y = int(input())
+    drawing(x, y)
