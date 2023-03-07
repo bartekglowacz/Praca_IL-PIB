ca01207d62bb1c72838c2c5db67ed1ba3d43731a
bartekglowacz <bartekglowacz@gmail.com>
2023-03-07 15:35:32 +0100

    .

--- tmp_roboczy.py
+++ tmp_roboczy.py
@@ -1,26 +1,34 @@
-import datetime
+import random
+import statistics as stat
 
-import SMF100A_ESU40
+with open("C:\\Users\\bglowacz\\PycharmProjects\\Praca_IL-PIB\\frequencies_txt", "r") as frequencies_file:
+    frequencies = frequencies_file.readlines()
 
+frequencies = [float(x) for x in frequencies]
+level = []
+result_list = []
 
-def result_file_name(name_of_file):
-    now = datetime.datetime.now()
-    year = str(now.year)
-    month = "%02d" % now.month
-    day = "%02d" % now.day
-    hour = "%02d" % now.hour
-    minute = "%02d" % now.minute
-    second = "%02d" % now.second
-    prefix_name = year + month + day + "_" + hour + minute + second + "_"
-    full_name_of_file = prefix_name + name_of_file + ".txt"
-    return full_name_of_file
+for x in frequencies:
+    random_number = random.randint(1, 100)
+    level.append(random_number)
+print("Oryginalne poziomy: ", level)
 
+standard_deviation = stat.pstdev(level)
+print(f"Odchylenie standardowe wynosi: {standard_deviation}")
+median = stat.median(level)
+print(f"Mediana wynosi: {median}")
 
-print("Wprowadź nazwę pliku: ")
-file_name = input()
-final_file_name = result_file_name(file_name)
-print(final_file_name)
-final_results_txt = open(f"{final_file_name}", "w")
+for x in range(0, len(frequencies)):
+    while level[x] < median:
+        random_number = random.randint(1, 100)
+        level[x] = random_number
+        str(level[x]).replace(str(x), str(random_number))
 
-for value in SMF100A_ESU40_v2.frequency:
-    final_results_txt.write("f [MHz]\tU [dBuV]")
+    result_list.append(str(frequencies[x]) + "  " + str(level[x]) + "")
+print(f"Nowe poziomy, z zamienionymi wartościami poniżej mediany: {level}")
+print(result_list)
+
+result_file = open("result.txt", "w")
+for x in range(0, len(frequencies)):
+    result_file.write(result_list[x])
+
