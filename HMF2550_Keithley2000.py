e40e9843a8c5a0b181b1aab742a718a5aabec26b
bartekglowacz <bartekglowacz@gmail.com>
2023-04-12 15:47:58 +0200

    Czasami odczytuje duże odchylenia od wartości średniej

--- HMF2550_Keithley2000.py
+++ HMF2550_Keithley2000.py
@@ -1,3 +1,6 @@
+import datetime
+import math
+import time
 import pyvisa
 
 
@@ -24,55 +27,122 @@ class Voltmeter(Device):
         super().__init__(address, name)
 
     def AC_or_DC(self):
+        self.connected_device_name.write("*RST")
         print("Wybierz tryb AC lub DC wpisując opowiednio te skróty")
         choice = input().upper()
         if choice == "AC":
-            self.connected_device_name.write('FUNC "VOLT:AC"')
-            self.connected_device_name.write("UNIT:VOLT:AC dBm")
+            self.connected_device_name.write("FUNC 'VOLT:AC'")
+            self.connected_device_name.write("UNIT:VOLT:AC V")
         elif choice == "DC":
             self.connected_device_name.write('FUNC "VOLT:DC"')
-            self.connected_device_name.write("UNIT:VOLT:DC dBm")
+            self.connected_device_name.write("UNIT:VOLT:DC V")
         else:
             print("Dokonano złego wyboru")
 
-    def read_level(self):
-        self.connected_device_name.write("INIT:CONT OFF")
-        level = float(self.connected_device_name.query("read?"))
-        return level
+    def RMS_or_Peak(self, Urms, type_of_result):
+        if type_of_result.upper() == "P":
+            Up = Urms * math.sqrt(2)
+            return Up
+        elif type_of_result.upper() == "RMS":
+            return Urms
+        else:
+            print("Nieprawidłowy wybór!")
+
+    def read_level(self, freq):
+        self.connected_device_name.write("*RST")
+        # self.connected_device_name.write(":SENS:VOLT:AC:RANG:AUTO OFF")  # ręcznie ustawienie zakresu woltomierza
+        # self.connected_device_name.write(":SENS:VOLT:AC:RANG 1")  # ręcznie ustawienie zakresu woltomierza
+        self.connected_device_name.write(":SENS:VOLT:AC:RANG:AUTO ON")
+        if freq < 1:
+            self.connected_device_name.write("FUNC 'VOLT:DC'")
+            self.connected_device_name.write("UNIT:VOLT:DC V")
+            level_voltmeter = float(self.connected_device_name.query("read?"))
+            return level_voltmeter
+        if freq >= 1:
+            self.connected_device_name.write("FUNC 'VOLT:AC'")
+            self.connected_device_name.write("UNIT:VOLT:AC V")
+            level_voltmeter = float(self.connected_device_name.query("read?"))
+            return level_voltmeter
 
 
 class SignalGenerator(Device):
     def __init__(self, address, name):
         super().__init__(address, name)
 
-    def set_frequency_band(self):
-        file = open("frequencies_txt", "r")
-        frequencies = file.readlines()
-        frequencies = [float(freq.replace(",", ".")) for freq in frequencies]
-        print(frequencies)
-        return frequencies
-
-    def set_level(self, level):
-        self.connected_device_name.write("VOLT:UNIT DBM")
-        self.connected_device_name.write(f":VOLT {level}")
+    def set_level(self, level_func):
+        self.connected_device_name.write("VOLT:UNIT VOLT")
+        self.connected_device_name.write(f":VOLT {level_func}")
 
     def power_on_off(self, mode):
-        self.connected_device_name.write(f"OUTP {mode}")
+        self.connected_device_name.write(f"OUTP {mode.upper()}")
 
-    def set_single_frequency(self):
-        self.connected_device_name.write("FREQ 100")
+    def set_single_frequency(self, f):
+        self.connected_device_name.write(f"FREQ {f}")
+        return f
 
 
-# keithley2000 = Voltmeter('GPIB0::16::INSTR', "Keithley")
-# keithley2000.connect()
+def set_frequency_band():
+    file = open("frequencies_txt", "r")
+    frequencies = file.readlines()
+    frequencies = [float(freq.replace(",", ".")) for freq in frequencies]
+    return frequencies
+
+
+def result_file_name(name, result_list):
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = "%02d" % now.month
+    day = "%02d" % now.day
+    hour = "%02d" % now.hour
+    minute = "%02d" % now.minute
+    second = "%02d" % now.second
+    prefix_name = year + month + day + "_" + hour + minute + second + "_"
+    full_name_of_file = prefix_name + name + ".txt"
+    result_txt = open(f"C:\\Users\\bglowacz\\PycharmProjects\\Praca_IL-PIB\\pliki wynikowe txt\\{full_name_of_file}",
+                      "w")
+    result_txt.write("f [Hz]\tU [V]\n")
+    for x in result_list:
+        result_txt.write(x.replace(".", ",") + "\n")
+    result_txt.close()
+
+
+keithley2000 = Voltmeter('GPIB0::16::INSTR', "Keithley")
+keithley2000.connect()
+keithley2000.IDN()
 # keithley2000.AC_or_DC()
-# keithley2000.IDN()
-# keithley2000.read_level()
 
 HMF2550 = SignalGenerator("ASRL10::INSTR", "HMF2550")
 HMF2550.connect()
 HMF2550.IDN()
-HMF2550.set_frequency_band()
 HMF2550.power_on_off("ON")
-HMF2550.set_level(10)
-HMF2550.set_single_frequency()
+print("Podaj napięcie generatora: ")
+generator_level = float(input())
+HMF2550.set_level(generator_level)
+frequency_list = set_frequency_band()
+
+result = []
+print("Wpisz:\np - dla rezultatu peak\nrms - dla rezultatu w RMS")
+p_rms = input()
+print("Wpisz:\ndB - dla wyniku z dBuV\nV - dla wyniku w V")
+dB_V = input()
+
+if dB_V not in ["V", "dB"]:
+    print("Nieprawidłowy wybór")
+    exit()
+for f in frequency_list:
+    frequency = float(HMF2550.set_single_frequency(f))
+    print(f"f generatora = {frequency}")
+    time.sleep(0.200)
+    level = abs(float(keithley2000.read_level(f)))
+    level = keithley2000.RMS_or_Peak(level, p_rms)  # p dla wartości peak, rms dla wartości rms
+    if dB_V == "dB":
+        level = 20 * math.log(level * pow(10, 6), 10)
+    print(f"U = {level}")
+    result.append(str(frequency) + "\t" + str(level))
+
+print(result)
+HMF2550.power_on_off("off")
+
+print("Nazwa pliku:")
+file_name = input()
+result_file_name(file_name, result)
